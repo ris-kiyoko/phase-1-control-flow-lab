ab6d5c5d71fcb55f30fe7a779fdd593519470aa6
Marisa <marisae05@gmail.com>
2022-07-19 22:51:32 -0700

    commit tree 08680a6fc3952ca91a386bc5d3fed21554c159d6

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+if (distance <= 400){
+  return 'This one is on me!'
+} else if (distance > 400 && distance < 2000){
+  return 'That will be twenty bucks.'
+} else if (distance > 2500){
+  return 'No can do.'
+} else if (distance > 2000){
+  return 'I will gladly take your thirty bucks.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity(city){
+return (city === "NYC") ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+
+
+
+function switchOnCharmFromTip(money){
+ //  if money === "generous"  // "Thank you so much. "
+ // if money === "not as generous" // "Thank you."
+ // if money === "thanks for everything" // "Bye."
+ 
+switch(money){
+  case "generous":
+    return "Thank you so much."
+  case "not as generous":
+    return "Thank you."
+  case "thanks for everything":
+    return "Bye."
+}
+
+
+
   // Write your code here!
+
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -2,6 +2,7 @@ describe('index.js', function () {
   describe('scuberGreetingForFeet()', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
       expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+      expect(scuberGreetingForFeet(400)).to.equal('This one is on me!');
     });
 
     it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
